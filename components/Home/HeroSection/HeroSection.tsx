192de4987364ac190925834e337efe364685c7ea
Kharisma Fajar <restuistyo@gmail.com>
2025-09-10 09:50:28 +0700

    responsif hero

--- components/Home/HeroSection/HeroSection.tsx
+++ components/Home/HeroSection/HeroSection.tsx
@@ -12,23 +12,27 @@ const HeroSection = () => {
       <div className="container h-full flex">
         <div className="grid grid-cols-12 items-center h-full">
           <div className="col-span-12 lg:col-span-6">
-            <div className="flex flex-col gap-5">
-              <h1 className="text-6xl font-semibold leading-[130%]">
-                Kendaraan jadi <span className="text-primary">kinclong,</span>{" "}
+            <div className="flex flex-col gap-3 lg:gap-5">
+              <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold leading-[130%]">
+                Kendaraan jadi <span className="text-primary">kinclong,</span>
+                <br />
                 tanpa bikin kantong kosong
               </h1>
-              <p className="font-secondary text-xl/9 text-secondary">
+              <p className="font-secondary text-lg lg:text-xl/9 text-secondary">
                 Percayakan mobil & motor Anda pada layanan cuci profesional
                 kami. Bersih maksimal, aman untuk body kendaraan, dan cepat
                 selesai.
               </p>
-              <div className="button-hero flex gap-4">
-                <Link href="#" className="btn btn-primary">
+              <div className="button-hero flex flex-col md:flex-row gap-2 lg:gap-4">
+                <Link
+                  href="#"
+                  className="btn btn-primary hover-up text-center md:text-start"
+                >
                   Pesan Sekarang
                 </Link>
                 <Link
                   href="#"
-                  className="btn btn-secondary flex items-center gap-1"
+                  className="btn btn-secondary flex items-center justify-center gap-1 hover-up"
                 >
                   Layanan Kami <GrFormNextLink className="inline" size={20} />
                 </Link>
@@ -51,17 +55,28 @@ const HeroSection = () => {
               <Image
                 src={"/images/hero-images.png"}
                 alt="Deril Carwash"
-                className="rounded-lg"
+                className="rounded-lg w-full h-70 md:h-90 lg:h-180 object-cover"
                 width={1000}
                 height={1000}
               ></Image>
-              <div className="btn btn-secondary absolute flex items-center gap-1 -top-6 -right-5">
-                <LuShieldCheck size={20} className="text-primary" />
-                Garansi Bersih
+              <div className="absolute gap-1 -top-6 -right-1 lg:-right-5">
+                <div className="relative">
+                  <div className="absolute inset-0.5 bg-primary rounded-[6px] blur opacity-50 animate-fade-in-scale"></div>
+                  <div className="btn btn-secondary text-sm md:text-base flex items-center relative">
+                    <LuShieldCheck size={20} className="text-primary" />
+                    Garansi Bersih
+                  </div>
+                </div>
               </div>
-              <div className="btn btn-secondary absolute flex items-center gap-1 -bottom-6 -left-5">
-                <FiThumbsUp size={20} className="text-primary" />
-                Layanan Memuaskan
+
+              <div className="absolute -bottom-6 -left-1 lg:-left-5">
+                <div className="relative">
+                  <div className="absolute inset-0.5 bg-primary rounded-[6px] blur opacity-50 animate-fade-in-scale"></div>
+                  <div className="btn btn-secondary text-sm md:text-base flex items-center gap-1 relative">
+                    <FiThumbsUp size={20} className="text-primary" />
+                    Layanan Memuaskan
+                  </div>
+                </div>
               </div>
             </div>
           </div>
